71282346d95ac52d5edeb7ca00dcb71ea25d6052
Matus Marko <matus.marko@vaimo.com>
2018-01-12 21:29:42 +0200

    rest controller update to new connector model

--- rest/src/main/java/search/openweathermap/WeatherRestController.java
+++ rest/src/main/java/search/openweathermap/WeatherRestController.java
@@ -1,8 +1,8 @@
 package search.openweathermap;
 
+import org.openweathermap.CurrentType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import weather.Current;
 
 @RestController
 @RequestMapping("/owm/weather")
@@ -10,12 +10,12 @@ public class WeatherRestController {
 
     @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public Current search(@RequestParam(value = "token") String token) {
+    public CurrentType search(@RequestParam(value = "token") String token) {
 
         RestTemplate template = new RestTemplate();
         String url = "http://api.openweathermap.org/data/2.5/weather?q={token}&mode=xml&units=metric&APPID={apikey}";
         String apiKey = "e6b0d19165248d97ae9b26534222d33e";
-        Current current = template.getForObject(url, Current.class, token, apiKey);
+        CurrentType current = template.getForObject(url, CurrentType.class, token, apiKey);
         return current;
     }
 }
