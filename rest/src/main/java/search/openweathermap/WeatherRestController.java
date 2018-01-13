11e2a22ad39426de2fe7a76f2093194a92ee8caa
Matus Marko <matus.marko@vaimo.com>
2018-01-13 10:37:16 +0200

    api key loading

--- rest/src/main/java/search/openweathermap/WeatherRestController.java
+++ rest/src/main/java/search/openweathermap/WeatherRestController.java
@@ -1,6 +1,9 @@
 package search.openweathermap;
 
 import org.openweathermap.CurrentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,14 +11,33 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/owm/weather")
 public class WeatherRestController {
 
+    private static final Logger log = LoggerFactory.getLogger(WeatherRestController.class);
+
+    @Value("${openweathermap.key}")
+    private String apiKey;
+
+    @Value("${openweathermap.sample}")
+    private String isSample;
+
     @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public CurrentType search(@RequestParam(value = "token") String token) {
 
+        String sampleUrl
+                = "http://samples.openweathermap.org/data/2.5/weather?q={token}&mode=xml&APPID={apikey}";
+
+        String productionUrl
+                = "http://api.openweathermap.org/data/2.5/weather?q={token}&mode=xml&units=metric&APPID={apikey}";
+
+        log.info("api key: " + this.apiKey);
+        log.info("api isSample: " + this.isSample);
+
         RestTemplate template = new RestTemplate();
-        String url = "http://api.openweathermap.org/data/2.5/weather?q={token}&mode=xml&units=metric&APPID={apikey}";
-        String apiKey = "e6b0d19165248d97ae9b26534222d33e";
-        CurrentType current = template.getForObject(url, CurrentType.class, token, apiKey);
-        return current;
+        return template.getForObject(
+                ("true".equals(this.isSample)) ? sampleUrl : productionUrl,
+                CurrentType.class,
+                token,
+                this.apiKey
+        );
     }
 }
