8bf375e04fe6d91b30fd2be681cddc579c088f6d
Matus Marko <matus.marko@vaimo.com>
2018-01-14 20:44:56 +0200

    Favourites frontend impl

--- frontend/public/rest-script.js
+++ frontend/public/rest-script.js
@@ -1,16 +1,47 @@
-$('#search-btn').on('click', function() {
+$(document).ready(function() {
+    renderFavourites();
+});
+
+$(document).on('click', '#search-btn', function() {
     var token = $('#search-input').val();
     $.ajax({
         url: 'http://localhost:8080/owm/weather',
         data: {token: token}
     }).done(function(data, textStatus, jqXHR) {
-        $('.result').html(preRenderItem(data));
+        $('.result').html(preRenderResultItem(data));
     }).fail(function(jqXHR, textStatus, errorThrown) {
        $('.result').html('');
-   });
+    });
 });
 
-function preRenderItem(data) {
+$(document).on('click', '#add-to-favourites', function(event) {
+    var city = $(this).data('value');
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/owm/favourites',
+        data: {city: city},
+        dataType: 'json'
+    }).always(function(data, textStatus, jqXHR) {
+        renderFavourites()
+    });
+});
+
+$(document).on('click', '.favourite-remove', function() {
+    var id = $(this).data('value');
+    $.ajax({
+        type: 'DELETE',
+        url: 'http://localhost:8080/owm/favourites/' + id
+    }).always(function(data, textStatus, jqXHR) {
+        renderFavourites()
+    });
+});
+
+$(document).on('click', '.favourite-search', function() {
+    $('#search-input').val($(this).text());
+    $('#search-btn').trigger('click');
+});
+
+function preRenderResultItem(data) {
     var item = '';
     item += '<div class="row">';
 
@@ -18,7 +49,7 @@ function preRenderItem(data) {
     item += '<image src="http://openweathermap.org/img/w/' + data.weather.icon + '.png" />';
     item += '</div>';
 
-    item += '<div class="col-md-10">';
+    item += '<div class="col-md-9">';
 
     item += '<div class="row">';
 
@@ -47,10 +78,47 @@ function preRenderItem(data) {
 
     item += '</div>';
 
+    item += '<div class="col-md-2">';
+    item += '<button type="button" class="btn btn-info" id="add-to-favourites"';
+    item += 'data-value="' + data.city.name + '">Add to favourites</button>';
+    item += '</div>';
+
     item += '</div>';
 
     return item;
 };
+
+function clearFavourites() {
+    $('.favourite-item').remove();
+}
+
+function renderFavourites() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/owm/favourites'
+    }).done(function(data, textStatus, jqXHR) {
+        clearFavourites();
+        $.each(data, function(i, obj) {
+            $('.favourites-wrapper').append(preRenderFavouriteItem(obj));
+        })
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        clearFavourites();
+    });
+}
+
+function preRenderFavouriteItem(data) {
+    var item = '';
+    item += '<div class="row favourite-item" style="padding-bottom: 15px">';
+    item += '<div class="btn-group pull-right" role="group">';
+    item += '<button type="button" class="btn btn-default favourite-search">' + data.city + '</button>';
+    item += '<button type="button" class="btn btn-default favourite-remove" data-value="' + data.id + '">';
+    item += '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
+    item += '</button>';
+    item += '</div>';
+    item += '</div>';
+
+    return item;
+}
 
 function formatUnit(unit) {
     switch (unit) {
